d374b11d9fe7c1cfdbc8f04f3a0c333b2cf89d69
Rene90 <rob_rene199027@hotmail.com>
2022-11-28 17:23:51 -0700

    Final

---
+++ 01Ecmascript/algoritmo.py
@@ -0,0 +1,13 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-l64 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Ingrese el primer numero")
+	a = input()
+	print("Ingrese el segundo elemento")
+	b = input()
+	c = a+b
+	print("El resultado es: ",c)
+
--- 01Ecmascript/main.js
+++ 01Ecmascript/main.js
@@ -9,10 +9,20 @@ for (let i=0; i<arreglo.length;i++){
 
 //rest operator
 function sumacalculadora(...rest){
-    
+    let sumatoria =0
+    for (let i=0;i<rest.length;i++){
+        sumatoria +=rest[i]
+    }
+    return sumatoria
 
 }
-
+//operador spread
+let ejemploSpread = [2,6,4,2,3]
+//sumacalculadora(ejemploSpread)//Esto me marca error porque la funcion no recibe un arreglo, sino parametros 
+sumacalculadora(...ejemploSpread)//Esto si funciona
+//Template literals ECMASCRIPT 6
+var resultadoRest = sumacalculadora(2,5,7,8,9)
+console.log(`El resultado de la suma con el operador rest es ${resultadoRest}`)
 //Estas dos funciones suman N cantidad de elementos
 var arregloNumeros = []
 function adicionarNumero(arr, num){
@@ -45,3 +55,33 @@ const sumarCalcu=()=>{
     alert(resultado)
 
 }
+//Operador spread
+//Objetos json
+const obj1 = { foo: 'bar', x: 42 };
+const obj2 = { foo: 'baz', y: 13 };
+
+const clonedObj = { ...obj1 };
+// { foo: "bar", x: 42 }
+console.log(clonedObj)
+
+
+const mergedObj = { ...obj1, ...obj2 };
+console.log(mergedObj)
+const ejemplo = ()=>{
+    //objeto json javascript object notation 
+    alumno1 = {
+        nombre:"Martin",
+        edad:16
+    }
+    return alumno1
+}
+var resultadoRolando = ejemplo()
+var nombreObjeto = resultadoRolando.nombre
+var edadObjeto = resultadoRolando.edad
+
+
+
+
+//deconstruir un objeto json
+({nombre,edad} = ejemplo())
+console.log(nombre)
